05fac455d2dde7b887238197903cce6ff79ce9dc
Ida Iyes <identry@protonmail.com>
2023-07-24 12:04:57 +0300

    refactor appstate stuff, add clicommand for switching

---
+++ src/appstate.rs
@@ -0,0 +1,57 @@
+use bevy::reflect::{DynamicEnum, DynamicVariant};
+
+use crate::prelude::*;
+
+pub struct AppStatesPlugin;
+
+impl Plugin for AppStatesPlugin {
+    fn build(&self, app: &mut App) {
+        // our states
+        app.add_state::<AppState>();
+        // TODO: replace with OnTransition (bevy 0.11)
+        for state in enum_iterator::all::<AppState>() {
+            app.add_system(
+                despawn_all_recursive::<With<StateDespawnMarker>>.in_schedule(OnExit(state)),
+            );
+        }
+        app.register_clicommand_args("AppState", cli_appstate);
+    }
+}
+
+/// State type: Which "screen" is the app in?
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, Default, States)]
+#[derive(Reflect, FromReflect)]
+#[derive(enum_iterator::Sequence)]
+pub enum AppState {
+    /// Initial loading screen at startup
+    #[default]
+    AssetsLoading,
+    /// Main Menu
+    MainMenu,
+    /// Gameplay
+    InGame,
+}
+
+/// Marker for entities that should be despawned on `AppState` transition.
+///
+/// Use this on entities spawned when entering specific states, that need to
+/// be cleaned up when exiting.
+#[derive(Component)]
+pub struct StateDespawnMarker;
+
+/// CliCommand for switching state
+fn cli_appstate(In(args): In<Vec<String>>, mut next: ResMut<NextState<AppState>>) {
+    if args.len() != 1 {
+        error!("\"appstate <Value>\"");
+        return;
+    }
+
+    let type_name = AppState::MainMenu.type_name();
+
+    let dyn_state = DynamicEnum::new(type_name, &args[0], DynamicVariant::Unit);
+    if let Some(state) = FromReflect::from_reflect(&dyn_state) {
+        next.set(state);
+    } else {
+        error!("Invalid app state: {}", args[0]);
+    }
+}
