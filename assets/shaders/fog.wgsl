abe800d5b11ceb9f3d9260d91f2428fb3cc8a3b0
destravous <destravous@gmail.com>
2024-02-20 22:15:57 -0500

    multilayered fog!

--- assets/shaders/fog.wgsl
+++ assets/shaders/fog.wgsl
@@ -4,6 +4,8 @@
 #import "shaders/perlin_noise_3d.wgsl"::perlinNoise3
 
 struct FogMaterial {
+    depth: f32,
+    alpha: f32,
     color: vec4<f32>,
     emitter1: vec4<f32>,
     emitter2: vec4<f32>,
@@ -19,7 +21,7 @@ fn fragment(mesh: VertexOutput) -> @location(0) vec4<f32> {
     let camera_pos = view.world_position;
     let emitter_pos_init = fog_mat.emitter1.xy;
     var delta = emitter_pos_init - camera_pos.xy;
-    delta *= 1.0/ 0.7;
+    delta *= 1.0/ (fog_mat.depth);
     let emitter_pos_final = camera_pos.xy + delta.xy;
     let offset = emitter_pos_final - emitter_pos_init;
     let distance = distance(world_pos, emitter_pos_final);
@@ -27,16 +29,15 @@ fn fragment(mesh: VertexOutput) -> @location(0) vec4<f32> {
 
     // Normalize the distance to use it for a gradient effect
     // Assuming a maximum distance that makes sense for your use case, for example, 1000 units
-    let max_distance = 50.0;
+    let max_distance = fog_mat.emitter1.z;
     let scaledpos = (world_pos - offset)*0.05;
-    var noise = perlinNoise3(vec3<f32>(globals.time*0.3, scaledpos.y, scaledpos.x, ));
-    var noise1 = perlinNoise3(vec3<f32>(globals.time*0.3, scaledpos.y*0.5, scaledpos.x*0.5, ));
+    var noise = perlinNoise3(vec3<f32>(globals.time*0.3 / fog_mat.depth, scaledpos.y, scaledpos.x, ));
+    var noise1 = perlinNoise3(vec3<f32>(globals.time*0.3 / fog_mat.depth, scaledpos.y*0.5, scaledpos.x*0.5, ));
     noise = (noise*noise - noise1)*0.3 + 0.5;
     let normalized_distance = clamp((distance / max_distance) * noise, 0.0, 1.0) ;
 
-    // Create a gradient based on the distance. This example modifies the alpha value,
-    // but you could also adjust the color intensity or create a color gradient
-    let gradient_color = vec4<f32>(white.rgb, (1.0 - normalized_distance) *0.25); // Fading effect
+    // Create a gradient based on the distance.
+    let gradient_color = vec4<f32>(white.rgb, (1.0 - normalized_distance) *fog_mat.alpha); // Fading effect
 
     return gradient_color;
     //return ;
--- game/src/graphics.rs
+++ game/src/graphics.rs
@@ -16,7 +16,7 @@ impl Plugin for GraphicsFxPlugin {
 }
 
 #[derive(Component, Default)]
-pub struct FogEmitter;
+pub struct FogLayer;
 
 pub fn spawn_fog(
     mut commands: Commands,
@@ -24,9 +24,9 @@ pub fn spawn_fog(
     mut materials: ResMut<Assets<FogMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
-    println!("fog spawned");
+    // Spawn two layers
     commands.spawn((
-        FogEmitter,
+        FogLayer,
         MaterialMesh2dBundle {
             mesh: meshes
                 .add(Mesh::from(Quad::new(Vec2::splat(
@@ -35,20 +35,63 @@ pub fn spawn_fog(
                 .into(),
             transform: Transform::default().with_scale(Vec3::splat(1.0)),
             material: materials.add(FogMaterial {
+                depth: 0.7,
+                alpha: 0.066,
                 color: Color::BLUE,
-                emitter1: Vec4::new(0.0, 400.0, 0.0, 0.0),
+                emitter1: Vec4::new(0.0, 400.0, 50.0, 0.0),
                 emitter2: Default::default(),
                 emitter3: Default::default(),
             }),
             ..default()
         },
     ));
+    commands.spawn((
+        FogLayer,
+        MaterialMesh2dBundle {
+            mesh: meshes
+                .add(Mesh::from(Quad::new(Vec2::splat(
+                    1000.0,
+                ))))
+                .into(),
+            transform: Transform::default().with_scale(Vec3::splat(1.0)),
+            material: materials.add(FogMaterial {
+                depth: 0.95,
+                alpha: 0.066,
+                color: Color::BLUE,
+                emitter1: Vec4::new(0.0, 400.0, 50.0, 0.0),
+                emitter2: Default::default(),
+                emitter3: Default::default(),
+            }),
+            ..default()
+        },
+    ));
+    commands.spawn((
+        FogLayer,
+        MaterialMesh2dBundle {
+            mesh: meshes
+                .add(Mesh::from(Quad::new(Vec2::splat(
+                    1000.0,
+                ))))
+                .into(),
+            transform: Transform::default().with_scale(Vec3::splat(1.0)),
+            material: materials.add(FogMaterial {
+                depth: 1.1,
+                alpha: 0.066,
+                color: Color::BLUE,
+                emitter1: Vec4::new(0.0, 400.0, 50.0, 0.0),
+                emitter2: Default::default(),
+                emitter3: Default::default(),
+            }),
+            ..default()
+        },
+    ));
+    println!("fog spawned");
 }
 
 pub fn update_fog(
     mut commands: Commands,
-    mut fog_bundle_query: Query<(&mut Transform), With<FogEmitter>>,
-    mut q_cam: Query<&Transform, (With<MainCamera>, Without<FogEmitter>)>,
+    mut fog_bundle_query: Query<(&mut Transform), With<FogLayer>>,
+    mut q_cam: Query<&Transform, (With<MainCamera>, Without<FogLayer>)>,
 ) {
     let Some(cam_trnsfrm) = q_cam.iter().next() else {
         return;
@@ -62,9 +105,17 @@ pub fn update_fog(
 /// each emitter field: (x, y, max_fog, max_dist)
 #[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
 struct FogMaterial {
+    /// depth is used for the parallax affect, note that this is independent
+    /// of actual transform depth
+    #[uniform(0)]
+    depth: f32,
+    #[uniform(0)]
+    alpha: f32,
     #[uniform(0)]
     color: Color,
     #[uniform(0)]
+    /// x, y, are the coordinates of the emitter;
+    /// z is distance
     emitter1: Vec4,
     #[uniform(0)]
     emitter2: Vec4,
