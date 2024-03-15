5311f14fcc69ab44fed57f729ce8a79c0f23db15
otis <electricbuck@gmail.com>
2024-03-15 15:45:51 -0400

    remove uneccesary bounds, change to fnonce

--- game/src/game/gentstate.rs
+++ game/src/game/gentstate.rs
@@ -4,44 +4,54 @@ use bevy::ecs::component::SparseStorage;
 
 use crate::prelude::*;
 
-fn transition_from<T: Component + Send + Sync + 'static>(
+pub fn transition_from<T: Component>(
     mut query: Query<(Entity, &mut TransitionsFrom<T>)>,
     mut commands: Commands,
 ) {
     for (entity, mut trans) in query.iter_mut() {
-        for transition in &trans.transitions {
-            transition(entity, &mut commands);
-        }
-        //could decide to remove state + transitionsfrom here
         if !&trans.transitions.is_empty() {
+            let transitions = std::mem::take(&mut trans.transitions);
+            for transition in transitions {
+                transition(entity, &mut commands);
+            }
+            //could decide to remove state + transitionsfrom here
             commands.entity(entity).remove::<T>();
-            trans.transitions.clear();
         }
     }
 }
 
-pub trait Transitionable<T: GentState + Default> {
-    fn new_transition(_next: T) -> Box<dyn Fn(Entity, &mut Commands) + Send + Sync + 'static> {
-        Box::new(|entity, commands| {
-            commands
-                .entity(entity)
-                .insert(GentStateBundle::<T>::default());
+pub trait Transitionable<T: GentState> {
+    fn new_transition(next: T) -> Box<dyn FnOnce(Entity, &mut Commands) + Send + Sync> {
+        Box::new(move |entity, commands| {
+            commands.entity(entity).insert(GentStateBundle::<T> {
+                state: next,
+                transitions: TransitionsFrom::<T>::default(),
+            });
         })
     }
 }
 
 //common state
-#[derive(Component, Deref, DerefMut, Default)]
+#[derive(Component, Deref, DerefMut)]
 pub struct TransitionsFrom<T> {
-    t: PhantomData<T>,
+    pub marker: PhantomData<T>,
     #[deref]
-    pub transitions: Vec<Box<dyn Fn(Entity, &mut Commands) + Send + Sync>>,
+    pub transitions: Vec<Box<dyn FnOnce(Entity, &mut Commands) + Send + Sync>>,
+}
+
+impl<T: GentState> Default for TransitionsFrom<T> {
+    fn default() -> Self {
+        Self {
+            marker: PhantomData::<T>::default(),
+            transitions: Default::default(),
+        }
+    }
 }
 
 #[derive(Bundle, Default)]
-pub struct GentStateBundle<T: GentState + Default> {
-    state: T,
-    transitions: TransitionsFrom<T>,
+pub struct GentStateBundle<T: GentState> {
+    pub state: T,
+    pub transitions: TransitionsFrom<T>,
 }
 
 // States
@@ -50,21 +60,21 @@ pub struct GentStateBundle<T: GentState + Default> {
 // Impl Playerstate for each state
 // Impl Transitionable<T: GentState> for each state that that should be able to be transitioned
 // from by a state
-pub trait GentState: Component<Storage = SparseStorage> + Clone {}
+pub trait GentState: Component<Storage = SparseStorage> {}
 
-#[derive(Component, Default, Copy, Clone, Debug)]
+#[derive(Component, Default, Debug)]
 #[component(storage = "SparseSet")]
 pub struct Idle;
 impl GentState for Idle {}
 impl Transitionable<Running> for Idle {}
 
-#[derive(Component, Default, Copy, Clone, Debug)]
+#[derive(Component, Default, Debug)]
 #[component(storage = "SparseSet")]
 pub struct Running;
 impl GentState for Running {}
 impl Transitionable<Idle> for Running {}
 
-#[derive(Component, Default, Copy, Clone, Debug)]
+#[derive(Component, Default, Debug)]
 #[component(storage = "SparseSet")]
 pub struct Falling;
 impl GentState for Falling {}
@@ -72,7 +82,7 @@ impl Transitionable<Grounded> for Falling {}
 impl Transitionable<Running> for Falling {}
 impl Transitionable<Idle> for Falling {}
 
-#[derive(Component, Clone, Debug)]
+#[derive(Component, Debug)]
 #[component(storage = "SparseSet")]
 pub struct Jumping {
     current_air_ticks: u32,
@@ -91,7 +101,7 @@ impl GentState for Jumping {}
 impl Transitionable<Falling> for Jumping {}
 impl Transitionable<Grounded> for Jumping {}
 
-#[derive(Component, Default, Copy, Clone, Debug)]
+#[derive(Component, Default, Debug)]
 #[component(storage = "SparseSet")]
 pub struct Grounded;
 impl GentState for Grounded {}
@@ -99,7 +109,7 @@ impl GentState for Grounded {}
 impl Transitionable<Jumping> for Grounded {
     fn new_transition(
         _next: Jumping,
-    ) -> Box<dyn Fn(Entity, &mut Commands) + Send + Sync + 'static> {
+    ) -> Box<dyn FnOnce(Entity, &mut Commands) + Send + Sync> {
         Box::new(|entity, commands| {
             commands
                 .entity(entity)
@@ -112,7 +122,7 @@ impl Transitionable<Jumping> for Grounded {
 impl Transitionable<Falling> for Grounded {
     fn new_transition(
         _next: Falling,
-    ) -> Box<dyn Fn(Entity, &mut Commands) + Send + Sync + 'static> {
+    ) -> Box<dyn FnOnce(Entity, &mut Commands) + Send + Sync> {
         Box::new(|entity, commands| {
             commands
                 .entity(entity)
@@ -122,12 +132,12 @@ impl Transitionable<Falling> for Grounded {
     }
 }
 
-#[derive(Component, Default, Copy, Clone, Debug)]
+#[derive(Component, Default, Debug)]
 #[component(storage = "SparseSet")]
 pub struct Attacking;
 impl GentState for Attacking {}
 
-#[derive(Component, Default, Copy, Clone, Debug)]
+#[derive(Component, Default, Debug)]
 #[component(storage = "SparseSet")]
 pub struct Hitstun;
 impl GentState for Hitstun {}
