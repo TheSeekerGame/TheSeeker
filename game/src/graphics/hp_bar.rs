bddc26e665c7a07e30cdb018919dd22ba85ecc31
destravous <destravous@gmail.com>
2024-05-13 20:07:59 -0400

    initial wip implementation

--- game/src/graphics/hp_bar.rs
+++ game/src/graphics/hp_bar.rs
@@ -3,10 +3,11 @@
 // wwaaait, don't really need shaders for this; can just stack some rectangles; ez
 // especially since its something so simple
 
-use crate::prelude::{AppState, Component, OnExit, Update};
-use bevy::prelude::{App, Assets, ColorMaterial, Commands, Mesh, Plugin, Rectangle, ResMut};
-use bevy::sprite::{Material2dPlugin, Mesh2dHandle};
-use theseeker_engine::prelude::{OnEnter, Startup};
+use crate::game::attack::Health;
+use crate::prelude::Update;
+use bevy::prelude::*;
+use bevy::sprite::{MaterialMesh2dBundle, Mesh2dHandle};
+use glam::{Vec2, Vec3Swizzles};
 
 pub struct HpBarsPlugin;
 
@@ -24,11 +25,64 @@ impl Plugin for HpBarsPlugin {
 // or heck, dont even need an hp bar component on the main entity, since querys filter already
 
 #[derive(Component)]
-struct HpBar;
+struct HpBar(Mesh2dHandle);
+#[derive(Component)]
+struct HpBackground(Mesh2dHandle);
+
 fn run(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    entity_with_hp: Query<(Entity, &Children, &Health), (Without<HpBar>, Without<HpBackground>)>,
+    mut hp_bar_child: Query<(Entity, &mut Transform, HpBar), Without<Without<HpBackground>>>,
+    mut hp_backround_child: Query<(Entity, &Transform, HpBackground), Without<HpBar>>,
 ) {
-    let mesh_handle = Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0)));
+    let hp_bar_width = 15.0;
+    let hp_bar_height = 1.0;
+    let padding = 0.2;
+    for ((entity, children, hp)) in entity_with_hp.iter() {
+        let should_display_bar = hp.current != hp.max || hp.current == 0;
+        for &child in children.iter() {
+            let mut anchor_pt = None;
+            if let Ok((hp_bar_entity, transform, bar)) = hp_backround_child.get_mut(child) {
+                if !should_display_bar {
+                    commands.entity(entity).remove_children(&[hp_bar_entity]);
+                    commands.entity(hp_bar_entity).despawn();
+                }
+                anchor_pt = Some(transform.translation.xy());
+            }
+            if let Ok((hp_bar_entity, mut transform, bar)) = hp_bar_child.get_mut(child) {
+                if !should_display_bar {
+                    commands.entity(entity).remove_children(&[hp_bar_entity]);
+                    commands.entity(hp_bar_entity).despawn();
+                } else {
+                    let scale = hp.current as f32 / hp.max as f32;
+                    transform.scale.x = scale;
+                    transform.translation.x = anchor_pt.unwrap().x - scale * 0.5 + padding;
+                }
+            }
+            // get the health of each child unit
+            let health = q_child.get(child);
+        }
+    }
+    let hp_bar_mesh_handle = Mesh2dHandle(meshes.add(Rectangle::new(
+        hp_bar_width,
+        hp_bar_height,
+    )));
+    let hp_bg_mesh_handle = Mesh2dHandle(meshes.add(Rectangle::new(
+        hp_bar_width,
+        hp_bar_height,
+    )));
+    let bg_color = Color::rgb(0.0, ));
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: hp_bar_mesh_handle,
+        material: materials.add(color),
+        transform: Transform::from_xyz(
+            // Distribute shapes from -X_EXTENT to +X_EXTENT.
+            -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
+            0.0,
+            0.0,
+        ),
+        ..default()
+    });
 }
