11eddef5c1d138085501ab130fa367bddf53be28
mnmaita <47983254+mnmaita@users.noreply.github.com>
2025-01-30 19:52:24 +0100

    Sets PlayerNearby slot for Merchant based on player proximity

--- game/src/game/merchant.rs
+++ game/src/game/merchant.rs
@@ -1,9 +1,13 @@
-use theseeker_engine::animation::SpriteAnimationBundle;
+use rapier2d::prelude::InteractionGroups;
 use theseeker_engine::assets::animation::SpriteAnimation;
-use theseeker_engine::gent::TransformGfxFromGent;
+use theseeker_engine::gent::{Gent, TransformGfxFromGent};
+use theseeker_engine::physics::{PhysicsWorld, PLAYER, SENSOR};
 use theseeker_engine::script::ScriptPlayer;
+use theseeker_engine::{animation::SpriteAnimationBundle, physics::Collider};
 
 use crate::prelude::*;
+
+use super::player::{Idle, Player};
 
 pub struct MerchantPlugin;
 
@@ -11,8 +15,18 @@ impl Plugin for MerchantPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
             GameTickUpdate,
-            (setup_merchant.run_if(in_state(GameState::Playing)))
-                .run_if(in_state(AppState::InGame)),
+            ((
+                setup_merchant
+                    .run_if(any_matching::<Added<MerchantBlueprint>>()),
+                merchant_proximity_to_player.run_if(any_matching::<(
+                    With<Player>,
+                    Without<Idle>,
+                )>()),
+            )
+                .run_if(
+                    in_state(GameState::Playing)
+                        .and_then(in_state(AppState::InGame)),
+                )),
         );
     }
 }
@@ -47,7 +61,22 @@ pub fn setup_merchant(
         xf_gent.translation.z = 13.0 * 0.000001;
         println!("{:?}", xf_gent);
         let e_gfx = commands.spawn(()).id();
-        commands.entity(e_gent).insert((Name::new("Merchant"),));
+        let e_effects_gfx = commands.spawn_empty().id();
+        commands.entity(e_gent).insert((
+            Name::new("Merchant"),
+            Gent {
+                e_gfx,
+                e_effects_gfx,
+            },
+            Collider::cuboid(
+                40.0,
+                40.0,
+                InteractionGroups {
+                    memberships: SENSOR,
+                    filter: PLAYER,
+                },
+            ),
+        ));
         let mut player = ScriptPlayer::<SpriteAnimation>::default();
         player.play_key("anim.merchant.Idle");
         commands.entity(e_gfx).insert((
@@ -65,5 +94,36 @@ pub fn setup_merchant(
             },
             StateDespawnMarker,
         ));
+    }
+}
+
+fn merchant_proximity_to_player(
+    merchant_query: Query<
+        (
+            Entity,
+            &Gent,
+            &GlobalTransform,
+            &Collider,
+        ),
+        With<MerchantBlueprint>,
+    >,
+    mut animation_query: Query<
+        &mut ScriptPlayer<SpriteAnimation>,
+        With<MerchantGfx>,
+    >,
+    spatial_query: Res<PhysicsWorld>,
+) {
+    for (entity, gent, transform, collider) in merchant_query.iter() {
+        let intersections = spatial_query.intersect(
+            transform.translation().xy(),
+            collider.0.shape(),
+            collider.0.collision_groups(),
+            Some(entity),
+        );
+
+        if let Ok(mut animation) = animation_query.get_mut(gent.e_gfx) {
+            let is_player_nearby = !intersections.is_empty();
+            animation.set_slot("PlayerNearby", is_player_nearby);
+        }
     }
 }
